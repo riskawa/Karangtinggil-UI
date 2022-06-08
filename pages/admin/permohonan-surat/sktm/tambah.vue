f0864ddd8a0696ea8f9f2590b2a1d64c8f0ca551
Riska Wilda <riskawildaa20@gmail.com>
2022-06-08 09:49:52 +0700

    UI Commit

---
+++ pages/admin/permohonan-surat/sktm/tambah.vue
@@ -0,0 +1,208 @@
+<template>
+    <v-col cols="12">
+        <v-card>
+            <v-toolbar flat>
+                <v-toolbar-title class="text-h5">
+                    Tambah Data Permohonan SKTM
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" to="/admin/permohonan-surat/sktm/">Kembali</v-btn>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-card-text>
+                <v-select v-if="$fetchState.pending"></v-select>
+                <v-select v-else :items="pemohons" label="Pilih Pemohon" item-text="nama" item-value="nik"
+                    v-model="select" @change="selectPemohon($event)">
+                </v-select>
+                <form @submit.prevent="submit" v-if="formVisible">
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Nama</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.nama" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">NIK</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.nik" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Jenis Kelamin</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.jenis_kelamin" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Tempat Lahir</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.tempat_lahir" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Tanggal Lahir</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.tanggal_lahir" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Kewarganegaraan</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.kewarganegaraan" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Agama</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.agama" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Alamat</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="pemohon.alamat" filled dense readonly>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2">Keperluan</v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-col cols="9">
+                            <v-text-field v-model="values.keperluan" :error-messages="errors.keperluan"
+                                @keypress="validate('keperluan')" @blur="validate('keperluan')" label="Keperluan" solo
+                                dense>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row class="d-flex align-center" no-gutters dense>
+                        <v-col cols="2"></v-col>
+                        <v-col cols="1"></v-col>
+                        <v-col cols="9">
+                            <v-btn :loading="isLoading" color="primary" type="submit">
+                                SIMPAN
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </form>
+            </v-card-text>
+        </v-card>
+    </v-col>
+</template>
+
+<script>
+import { object, string } from 'yup'
+
+const sktmSchema = object({
+    keperluan: string().required('Keperluan harus diisi'),
+})
+export default {
+    layout: 'admin',
+    data() {
+        return {
+            formVisible: false,
+            isLoading: false,
+            pemohons: [],
+            pemohon: {},
+            select: '',
+            values: {
+                keperluan: '',
+            },
+            errors: {
+                keperluan: '',
+            }
+        }
+    },
+    async fetch() {
+        this.$axios.$get('http://localhost:3333/pemohon/all')
+            .then(res => {
+                this.pemohons = res
+            })
+            .catch(() => {
+            })
+    },
+    methods: {
+        selectPemohon(evt) {
+            this.$axios.$get(`http://localhost:3333/pemohon/${evt}`)
+                .then(res => {
+                    this.pemohon = res.pemohon
+                    this.formVisible = true
+                })
+        },
+        validate(field) {
+            sktmSchema
+                .validateAt(field, this.values)
+                .then(() => {
+                    this.errors[field] = '';
+                })
+                .catch(err => {
+                    this.errors[field] = err.message
+                })
+        },
+        async submit() {
+            this.isLoading = true;
+            sktmSchema
+                .validate(this.values, { abortEarly: false })
+                .then(() => {
+                    this.errors = {};
+                    const fd = new FormData()
+                    fd.append('pemohonNik', this.select)
+                    fd.append('keperluan', this.values.keperluan)
+                    this.$axios.$post('http://localhost:3333/sktm ', fd)
+                        .then(() => {
+                            this.isLoading = false;
+                            const Toast = this.$swal.mixin({
+                                toast: true,
+                                position: 'top-end',
+                                showConfirmButton: false,
+                                showCloseButton: true,
+                                background: '#7C9885',
+                                color: 'white',
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                    toast.addEventListener('mouseenter', this.$swal.stopTimer)
+                                    toast.addEventListener('mouseleave', this.$swal.resumeTimer)
+                                }
+                            })
+
+                            Toast.fire({
+                                icon: 'success',
+                                title: 'Sukses tambah data SKTM'
+                            })
+                            this.$router.push('/admin/permohonan-surat/sktm')
+                        })
+                        .catch(({ response }) => {
+                            this.isLoading = false;
+                            this.errors = {};
+                            response.data.errors.map(e => {
+                                this.$toast.error(e.message, {
+
+                                });
+                            })
+                        })
+                })
+                .catch(err => {
+                    this.isLoading = false;
+                    err.inner.forEach(error => {
+                        this.errors[error.path] = error.message;
+                    });
+                });
+        }
+    }
+}
+</script>
---
+++ pages/kades/laporan/kehilangan-kk/index.vue
@@ -0,0 +1,326 @@
+<template>
+    <v-col cols="12" class="d-flex flex-column align-center justify-space-between">
+        <h1>Daftar Permohonan Surat Kehilangan KK</h1>
+        <v-col cols="6">
+            <v-card class="mb-2">
+                <v-card-text>
+                    <v-simple-table>
+                        <template v-slot:default>
+                            <tbody>
+                                <tr>
+                                    <td>Pilih Berdasarkan</td>
+                                    <td>:</td>
+                                    <td>
+                                        <v-select @change="gantiSort($event)" :items="items" item-text="text"
+                                            item-value="value" v-model="pilih" label="Pilih Berdasarkan" solo>
+                                        </v-select>
+                                    </td>
+                                </tr>
+                                <tr v-if="pilih == 1">
+                                    <td>Tanggal</td>
+                                    <td>:</td>
+                                    <td>
+                                        <v-menu v-model="menu" :close-on-content-click="false" :nudge-right="40"
+                                            transition="scale-transition" offset-y min-width="auto">
+                                            <template v-slot:activator="{ on, attrs }">
+                                                <v-text-field v-model="tanggal" label="Tanggal"
+                                                    prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on" solo>
+                                                </v-text-field>
+                                            </template>
+                                            <v-date-picker v-model="tanggal" @input="menu = false">
+                                            </v-date-picker>
+                                        </v-menu>
+                                    </td>
+                                </tr>
+                                <tr v-else-if="pilih == 2">
+                                    <td>Bulan</td>
+                                    <td>:</td>
+                                    <td>
+                                        <v-select :items="months" item-text="text" item-value="value" v-model="bulan"
+                                            solo label="Bulan"></v-select>
+                                    </td>
+                                </tr>
+                                <tr v-else-if="pilih == 3">
+                                    <td>Tahun</td>
+                                    <td>:</td>
+                                    <td class="">
+                                        <v-select :items="years" v-model="tahun" label="Tahun" solo>
+                                        </v-select>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-card-text>
+            </v-card>
+            <v-btn color="primary" @click="filterSurat">Tampilkan</v-btn>
+            <v-btn color="secondary" @click="reset">Reset</v-btn>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="10" class="d-flex justify-end">
+            <v-btn color="primary" @click="cetak">
+                <vueJsonExcelUmd class="btn btn-default" :data="json_data" :fields="json_fields"
+                    worksheet="My Worksheet" :name="filename">CETAK</vueJsonExcelUmd>
+            </v-btn>
+        </v-col>
+        <v-col cols="10">
+            <v-card>
+                <v-card-text>
+                    <v-col cols="12">
+                        <v-data-table :headers="headers" :items="kehilanganKks" disable-pagination
+                            :options.sync="options" :server-items-length="totalKehilanganKKs" :loading="loading"
+                            class="elevation-1 mb-2" :hide-default-footer="true">
+                        </v-data-table>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-row class="d-flex justify-between align-center">
+                            Tampilkan
+                            <v-col cols="1" sm="1">
+                                <v-select v-model="pageSize" :items="pageSizes" @change="handlePageSizeChange">
+                                </v-select>
+                            </v-col>
+                            <v-col cols="1">baris</v-col>
+                            <v-col cols="8" sm="8" class="d-flex justify-end">
+                                <v-pagination v-model="page" :length="totalPages" total-visible="7"
+                                    next-icon="mdi-menu-right" prev-icon="mdi-menu-left" @input="handlePageChange">
+                                </v-pagination>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-col>
+</template>
+
+<script>
+import { DateTime } from 'luxon'
+import vueJsonExcelUmd from 'vue-json-excel';
+
+export default {
+    layout: 'kades',
+    mounted() {
+        this.getKehilanganKKData()
+        const date = new Date()
+        const year = date.getFullYear()
+        this.years = this.getYear(year - 50)
+        this.generateFileName()
+    },
+    components: {
+        vueJsonExcelUmd
+    },
+    data() {
+        return {
+            filename: '',
+            items: [
+                {
+                    value: 1,
+                    text: "Per Tanggal"
+                },
+                {
+                    value: 2,
+                    text: "Per Bulan"
+                },
+                {
+                    value: 3,
+                    text: "Per Tahun"
+                },
+            ],
+            json_fields: {
+                No: 'no',
+                'Tanggal': 'tanggal',
+                'NIK': 'nik',
+                'Nama': 'nama',
+                'Keperluan': 'keperluan',
+                'Status': 'status'
+            },
+            json_data: [],
+            json_meta: [
+                [
+                    {
+                        key: 'charset',
+                        value: 'utf-8',
+                    },
+                ],
+            ],
+            totalKehilanganKKs: 0,
+            kehilanganKks: [],
+            loading: true,
+            options: {},
+            search: '',
+            headers: [
+                {
+                    text: 'No',
+                    align: 'start',
+                    sortable: false,
+                    value: 'no',
+                },
+                { text: 'Tanggal', value: 'tanggal' },
+                { text: 'NIK', value: 'nik' },
+                { text: 'Nama Lengkap', value: 'nama' },
+                { text: 'Status', value: 'status' },
+            ],
+            pageSize: 5,
+            pageSizes: [5, 10, 20, 50, 100],
+            page: 1,
+            totalPages: 0,
+            tanggal: '',
+            months: [
+                { text: 'Januari', value: 1 },
+                { text: 'Februari', value: 2 },
+                { text: 'Maret', value: 3 },
+                { text: 'April', value: 4 },
+                { text: 'Mei', value: 5 },
+                { text: 'Juni', value: 6 },
+                { text: 'Juli', value: 7 },
+                { text: 'Agustus', value: 8 },
+                { text: 'September', value: 9 },
+                { text: 'Oktober', value: 10 },
+                { text: 'November', value: 11 },
+                { text: 'Desember', value: 12 },
+            ],
+            years: [],
+            bulan: 0,
+            tahun: 0,
+            jenis_kelamin: ['Laki-laki', 'Perempuan'],
+            menu: false,
+            pilih: 0,
+            filterType: 0,
+            filter: '',
+        }
+    },
+    watch: {
+        options: {
+            handler() {
+                this.getKehilanganKKData()
+            },
+            deep: true,
+        },
+        search(value) {
+            this.search = value
+            this.page = 1
+            this.getKehilanganKKData()
+        }
+    },
+    methods: {
+        gantiSort(evt) {
+            this.pilih = evt
+        },
+        generateFileName() {
+            this.filename = `${DateTime.now().toISODate()}_KehilanganKK.xls`
+        },
+        async fetchData() {
+            const response = await this.$axios.$get(
+                'https://jsonplaceholder.typicode.com/users'
+            )
+            console.log(response)
+            return response
+        },
+        startDownload() {
+            alert('show loading')
+        },
+        finishDownload() {
+            alert('hide loading')
+        },
+        getYear(startYear) {
+            var currentYear = new Date().getFullYear(), years = [];
+            startYear = startYear || 1980;
+            while (currentYear >= startYear) {
+                years.push(currentYear--);
+            }
+            return years
+        },
+        async getKehilanganKKData() {
+            this.loading = true
+            await this.$axios.$get('http://localhost:3333/kehilangan-kk', {
+                params: {
+                    limit: this.pageSize,
+                    page: this.page - 1,
+                    search: this.search,
+                    filter: this.filter,
+                    filterType: this.filterType
+                }
+            }).then(res => {
+                this.getDisplayKehilanganKK(res)
+                this.totalKehilanganKKs = res.meta.total
+                this.loading = false
+                this.totalPages = res.meta.last_page
+            })
+        },
+        getDisplayKehilanganKK(data) {
+            this.json_data = data.data.map((kehilanganKk, i) => {
+                let no = (data.meta.current_page - 1) * data.meta.per_page + 1 + i
+                const tgl = DateTime.fromISO(kehilanganKk.created_at).toFormat('yyyy-LL-dd')
+                const status = (kehilanganKk.status == 1) ? 'Disetujui' : (kehilanganKk.status == 2) ? 'Surat Belum diambil' : (kehilanganKk.status == 3) ? 'Surat diambil' : 'Belum Diproses'
+                return {
+                    no: no,
+                    tanggal: tgl,
+                    nik: kehilanganKk.nik,
+                    nama: kehilanganKk.nama,
+                    keperluan: kehilanganKk.keperluan,
+                    status: status,
+                };
+            })
+            this.kehilanganKks = data.data.map((kehilanganKk, i) => {
+                let no = (data.meta.current_page - 1) * data.meta.per_page + 1 + i
+                const tgl = DateTime.fromISO(kehilanganKk.created_at).toFormat('yyyy-LL-dd')
+                const status = (kehilanganKk.status == 1) ? 'Disetujui' : (kehilanganKk.status == 2) ? 'Surat Belum diambil' : (kehilanganKk.status == 3) ? 'Surat diambil' : 'Belum Diproses'
+                return {
+                    no: no,
+                    id: kehilanganKk.id,
+                    nama: kehilanganKk.nama,
+                    status: status,
+                    nik: kehilanganKk.nik,
+                    tanggal: tgl
+                };
+            })
+        },
+        handlePageChange(value) {
+            this.page = value;
+            this.getKehilanganKKData();
+        },
+        handlePageSizeChange(size) {
+            this.pageSize = size;
+            this.page = 1;
+            this.getKehilanganKKData();
+        },
+        cetak() {
+            console.log(this.filename)
+        },
+        filterSurat() {
+            switch (this.pilih) {
+                case 1:
+                    this.filter = this.tanggal
+                    this.filterType = this.pilih
+                    this.page = 1
+                    this.getKehilanganKKData()
+                    break
+                case 2:
+                    this.filter = this.bulan
+                    this.filterType = this.pilih
+                    this.page = 1
+                    this.getKehilanganKKData()
+                    break
+                case 3:
+                    this.filter = this.tahun
+                    this.filterType = this.pilih
+                    this.page = 1
+                    this.getKehilanganKKData()
+                    break
+                default:
+                    break
+            }
+        },
+        reset() {
+            this.filter = ''
+            this.pilih = 0
+            this.filterType = 0
+            this.tanggal = ''
+            this.bulan = 0
+            this.tahun = 0
+            this.getKehilanganKKData()
+        },
+    },
+
+}
+</script>
