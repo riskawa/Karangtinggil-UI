d22b42db50b285daae78ab4b7d1d46cd8a0f836f
Riska Wilda <riskawildaa20@gmail.com>
2022-07-09 21:31:56 +0700

    Merubah view laporan

--- pages/kades/laporan/index.vue
+++ pages/kades/laporan/index.vue
@@ -14,10 +14,10 @@
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
                             <v-card to="/kades/laporan/pemohon" color="primary" dark :elevation="hover ? 8 : 4"
-                                height="175" class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                                height="140" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h6 text-center">
                                     Pemohon
                                 </v-card-text>
                             </v-card>
@@ -26,10 +26,10 @@
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
                             <v-card to="/kades/laporan/sktm" color="primary" dark :elevation="hover ? 8 : 4"
-                                height="175" class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                                height="140" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h6 text-center">
                                     SKTM
                                 </v-card-text>
                             </v-card>
@@ -38,10 +38,10 @@
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
                             <v-card to="/kades/laporan/skck" color="primary" dark :elevation="hover ? 8 : 4"
-                                height="175" class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                                height="140" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h6 text-center">
                                     SKCK
                                 </v-card-text>
                             </v-card>
@@ -52,10 +52,10 @@
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
                             <v-card to="/kades/laporan/domisili" color="primary" dark :elevation="hover ? 8 : 4"
-                                height="175" class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                                height="140" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h6 text-center">
                                     Surat Domisili
                                 </v-card-text>
                             </v-card>
@@ -63,11 +63,11 @@
                     </v-col>
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
-                            <v-card to="/kades/laporan/sku" color="primary" dark :elevation="hover ? 8 : 4" height="175"
-                                class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                            <v-card to="/kades/laporan/sku" color="primary" dark :elevation="hover ? 8 : 4" 
+                                height="140" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h6 text-center">
                                     SKU
                                 </v-card-text>
                             </v-card>
@@ -76,10 +76,10 @@
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
                             <v-card to="/kades/laporan/surat-keterangan" color="primary" dark :elevation="hover ? 8 : 4"
-                                height="175" class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                               height="140" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h6 text-center">
                                     Surat Keterangan
                                 </v-card-text>
                             </v-card>
@@ -90,10 +90,10 @@
                     <v-col cols="3">
                         <v-hover v-slot="{ hover }">
                             <v-card to="/kades/laporan/kehilangan-kk" color="primary" dark :elevation="hover ? 8 : 4"
-                                height="175" class="d-flex flex-column pa-4" shaped>
-                                <v-icon size=80>mdi-email</v-icon>
+                                height="160" class="d-flex flex-column pa-4" shaped>
+                                <v-icon size=70>mdi-email</v-icon>
                                 <!-- <v-img src="/surat.png" width="50"></v-img> -->
-                                <v-card-text class="d-flex justify-center white--text text-h5 text-center">
+                                <v-card-text class="d-flex justify-center white--text text-h7 text-center">
                                     Surat Kehilangan KK
                                 </v-card-text>
                             </v-card>
